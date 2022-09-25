ec8dbcfb7e61f47e16ed31d805f6e850f63acb3d
ehuang68 <>
2022-09-24 19:56:11 -0700

    connect to postgres instead of sqlite3

--- TodoApp/V2/database.py
+++ TodoApp/V2/database.py
@@ -3,10 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://postgres:password@localhost/TodoApplicationDatabase"
+)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    # connect_args={"check_same_thread": False}  # used by sqlite
 )
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine
