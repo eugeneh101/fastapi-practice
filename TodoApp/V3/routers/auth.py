d44e479dcc3d2308001c50e73f22a22ab692ec1f
ehuang68 <>
2022-10-02 19:43:13 -0700

    added new endpoints

--- TodoApp/V3/routers/auth.py
+++ TodoApp/V3/routers/auth.py
@@ -6,13 +6,15 @@ from datetime import datetime, timedelta
 from typing import Optional, Union
 
 from fastapi import (
-    #    FastAPI,
+    APIRouter,
     Depends,
     HTTPException,
+    Request,
     status,  # has a bunch of status codes
-    APIRouter,
 )
+from fastapi.responses import HTMLResponse
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from fastapi.templating import Jinja2Templates
 from jose import jwt, JWTError
 from pydantic import BaseModel
 from passlib.context import CryptContext
@@ -23,6 +25,7 @@ from database import SessionLocal, engine
 
 SECRET_KEY = "secret_key"
 ALGORITHM = "HS256"
+templates = Jinja2Templates(directory="templates")
 
 
 class CreateUser(BaseModel):
@@ -144,6 +147,16 @@ async def login_for_access_token(
         username=user.username, user_id=user.id, expires_delta=token_expires
     )
     return {"token": token}
+
+
+@router.get("/", response_class=HTMLResponse)
+async def authentication_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
 
 
 # Exceptions
