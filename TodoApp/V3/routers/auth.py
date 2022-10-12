20a804e4c77c6ad2155bf6f6dd6689e68830f95e
ehuang68 <>
2022-10-11 22:59:40 -0700

    updated registration page

--- TodoApp/V3/routers/auth.py
+++ TodoApp/V3/routers/auth.py
@@ -10,6 +10,7 @@ from starlette.responses import RedirectResponse
 from fastapi import (
     APIRouter,
     Depends,
+    Form,
     HTTPException,
     Response,
     Request,
@@ -70,11 +71,11 @@ def get_db():
         db.close()
 
 
-def get_password_hash(password) -> str:
+def get_password_hash(password: str) -> str:
     return bcrypt_context.hash(password)
 
 
-def verify_password(plain_password, hashed_password) -> bool:
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt_context.verify(plain_password, hashed_password)
 
 
@@ -210,6 +211,44 @@ async def logout(request: Request):
 @router.get("/register", response_class=HTMLResponse)
 async def register(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
+
+
+@router.post("/register", response_class=HTMLResponse)
+async def register_user(
+    request: Request,
+    email: str = Form(...),
+    username: str = Form(...),
+    firstname: str = Form(...),
+    lastname: str = Form(...),
+    password: str = Form(...),
+    password2: str = Form(...),
+    db: Session = Depends(get_db),
+):
+    validation1 = (
+        db.query(models.Users).filter(models.Users.username == username).first()
+    )
+    validation2 = db.query(models.Users).filter(models.Users.email == email).first()
+    if (
+        (password != password2)
+        or (validation1 is not None)
+        or (validation2 is not None)
+    ):
+        msg = "Invalid registration request"
+        return templates.TemplateResponse(
+            "register.html", {"request": request, "msg": msg}
+        )
+
+    user_model = models.Users()
+    user_model.username = username
+    user_model.email = email
+    user_model.first_name = firstname
+    user_model.last_name = lastname
+    user_model.hashed_password = get_password_hash(password=password)
+    user_model.is_active = True
+    db.add(user_model)
+    db.commit()
+    msg = "User successfully created"
+    return templates.TemplateResponse("login.html", {"request": request, "msg": msg})
 
 
 # Exceptions
